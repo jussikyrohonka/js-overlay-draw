db7e09281cc29c80137bc3c70263cd7b100e538e
Jussi Kyröhonka <jussi.kyrohonka@gmail.com>
2018-02-01 01:43:30 +0200

    Add an aws deploy script for S3 upload

---
+++ aws-deploy.sh
@@ -0,0 +1,12 @@
+
+
+aws s3api put-object --bucket jurmo.jussi.pro --content-type "text/html" --key index.html --body src/index.html
+aws s3api put-object --bucket jurmo.jussi.pro --content-type "application/javascript" --key jquery-3.2.1.min.js --body src/jquery-3.2.1.min.js
+aws s3api put-object --bucket jurmo.jussi.pro --content-type "application/javascript" --key jscolor.min.js --body src/jscolor.min.js
+aws s3api put-object --bucket jurmo.jussi.pro --content-type "application/javascript" --key page.js --body src/page.js
+aws s3api put-object --bucket jurmo.jussi.pro --content-type "text/css" --key styles.css --body src/styles.css
+aws s3api put-object --bucket jurmo.jussi.pro --content-type "image/png" --key jurmo-white.png --body src/jurmo-white.png
+aws s3api put-object --bucket jurmo.jussi.pro --content-type "image/png" --key jurmo-layer-48.png --body src/jurmo-layer-48.png
+aws s3api put-object --bucket jurmo.jussi.pro --content-type "image/png" --key jurmo-layer-49.png --body src/jurmo-layer-49.png
+aws s3api put-object --bucket jurmo.jussi.pro --content-type "image/png" --key jurmo-layer-62.png --body src/jurmo-layer-62.png
+aws s3api put-object --bucket jurmo.jussi.pro --content-type "image/png" --key jurmo-layer-79.png --body src/jurmo-layer-79.png
